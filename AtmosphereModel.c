262e5be167c6a5fba0268217f15ff0cef32784ee
oguyon <oliv.guyon@gmail.com>
2020-10-25 22:02:44 -1000

    first commit

---
+++ AtmosphereModel.c
@@ -0,0 +1,91 @@
+/**
+ * @file    AtmosphereModel.c
+ * @brief   Atmosphere model (refraction)
+ *
+ */
+
+
+#define _GNU_SOURCE
+
+/* ================================================================== */
+/* ================================================================== */
+/*  MODULE INFO                                                       */
+/* ================================================================== */
+/* ================================================================== */
+
+// module default short name
+// all CLI calls to this module functions will be <shortname>.<funcname>
+// if set to "", then calls use <funcname>
+#define MODULE_SHORTNAME_DEFAULT "atmmodel"
+
+// Module short description
+#define MODULE_DESCRIPTION       "Atmosphere model"
+
+
+
+
+
+
+/* ================================================================== */
+/* ================================================================== */
+/*  HEADER FILES                                                      */
+/* ================================================================== */
+/* ================================================================== */
+
+
+#include "CommandLineInterface/CLIcore.h"
+
+
+
+//
+// Forward declarations are required to connect CLI calls to functions
+// If functions are in separate .c files, include here the corresponding .h files
+//
+//#include "make_master_turbulence_screen.h"
+//#include "make_master_turbulence_screen_pow.h"
+
+
+
+
+
+
+/* ================================================================== */
+/* ================================================================== */
+/*  INITIALIZE LIBRARY                                                */
+/* ================================================================== */
+/* ================================================================== */
+
+// Module initialization macro in CLIcore.h
+// macro argument defines module name for bindings
+//
+INIT_MODULE_LIB(AtmosphericTurbulence)
+
+
+
+/**
+ * @brief Initialize module CLI
+ *
+ * CLI entries are registered: CLI call names are connected to CLI functions.\n
+ * Any other initialization is performed\n
+ *
+ */
+static errno_t init_module_CLI()
+{
+
+    //CLI_CMD_CONNECT("func1", "create_image_with_value");
+    
+    //AtmosphericTurbulence_make_master_turbulence_screen_addCLIcmd();
+	//AtmosphericTurbulence_make_master_turbulence_screen_pow_addCLIcmd();
+
+    // optional: add atexit functions here
+
+    return RETURN_SUCCESS;
+}
+
+
+
+
+
+
+
+
---
+++ AtmosphereModel.h
@@ -0,0 +1,17 @@
+/**
+ * @file    AtmosphereModel.h
+ * @brief   Function prototypes for AtmosphereModel functions
+ *
+ * Function prototypes are added here for use by other modules.\n
+ * Do not use this .h file for forward declaration internal to the module.\n
+ */
+
+
+
+#ifndef _ATMOSPHEREMODEL_H
+#define _ATMOSPHEREMODEL_H
+
+//#include "create_example_image.h"
+//#include "stream_process_loop_simple.h"
+
+#endif
